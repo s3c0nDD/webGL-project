a5d0cfebde6ff80b60e395b347f8564cec4bf239
s3c0nDD <s3c0nDD@users.noreply.github.com>
2016-09-05 13:24:06 +0200

    handling input mouse (or keyboards) moved to separate file

--- js/main.js
+++ js/main.js
@@ -42,45 +42,6 @@ function degToRad(degrees) {
     return degrees * Math.PI / 180;
 }
 
-var mouseDown = false;
-var lastMouseX = null;
-var lastMouseY = null;
-
-var moonRotationMatrix = mat4.create();
-mat4.identity(moonRotationMatrix);
-
-function handleMouseDown(event) {
-    mouseDown = true;
-    lastMouseX = event.clientX;
-    lastMouseY = event.clientY;
-}
-
-function handleMouseUp(event) {
-    mouseDown = false;
-}
-
-function handleMouseMove(event) {
-    if (!mouseDown) {
-        return;
-    }
-    var newX = event.clientX;
-    var newY = event.clientY;
-
-    var deltaX = newX - lastMouseX
-    var newRotationMatrix = mat4.create();
-    mat4.identity(newRotationMatrix);
-    mat4.rotate(newRotationMatrix, degToRad(deltaX / 10), [0, 1, 0]);
-
-    var deltaY = newY - lastMouseY;
-    mat4.rotate(newRotationMatrix, degToRad(deltaY / 10), [1, 0, 0]);
-
-    mat4.multiply(newRotationMatrix, moonRotationMatrix, moonRotationMatrix);
-
-    lastMouseX = newX
-    lastMouseY = newY;
-}
-
-
 function drawScene() {
     gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
